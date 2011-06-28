e497ff7add16aba516bd922f7584dc9f5145ead6
Munechika Sumikawa <sumikawa@sumikawa.jp>
2011-06-28 22:33:18 +0900

    default page

--- mysite/nw/urls.py
+++ mysite/nw/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('mysite.nw.views',
-    (r'^(?P<diag>[\d\w\/+=]+).png', 'show'),
-    (r'^edit/(?P<diag>[\d\w\/+=]+)$', 'edit'),
+    (r'^(?P<diag>[\d\w\/+=]*).png', 'show'),
+    (r'^edit/(?P<diag>[\d\w\/+=]*)$', 'edit'),
     (r'^json/$', 'json'),
 )
