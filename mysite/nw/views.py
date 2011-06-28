9d62c49cfb4c2658ef39250924720eb8ca5e20b9
Munechika Sumikawa <sumikawa@sumikawa.jp>
2011-06-28 22:32:52 +0900

    default page

--- mysite/nw/views.py
+++ mysite/nw/views.py
@@ -11,6 +11,8 @@ from django.views.decorators.cache import cache_page
 from ConfigParser import SafeConfigParser
 from nwdiag import diagparser, builder, DiagramDraw
 
+default_page = "QlpoOTFBWSZTWZtqPoAAAD9ZgEAQQABwCDavnJogAFQlTTSMaQNBmoNpKADR6gGkBefJp1hOwQrgW6hTWFQlmgUo2PqeEk6KuD6iLk9nFT6ZkZmdRQsPUwhj8XckU4UJCbaj6AA="
+
 @cache_page(60 * 15)
 def show(request, diag):
     tree = diagparser.parse(diagparser.tokenize(bz2.decompress(base64.b64decode(diag))))
@@ -24,6 +26,8 @@ def show(request, diag):
     return response
 
 def edit(request, diag):
+    if diag == '':
+	return HttpResponseRedirect(reverse('mysite.nw.views.edit', args=(default_page,)))
     plain = bz2.decompress(base64.b64decode(diag));
     return render_to_response('diag/edit.html', {'diag': diag, 'plain': plain})
 
