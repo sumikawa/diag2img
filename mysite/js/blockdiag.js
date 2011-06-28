cd8cb1ab31c7334f25d587dbe2474b9aee60404d
Munechika Sumikawa <sumikawa@sumikawa.jp>
2011-06-28 16:45:41 +0900

    resize textarea

--- mysite/js/blockdiag.js
+++ mysite/js/blockdiag.js
@@ -7,16 +7,6 @@ function genURL() {
     $('form#json').submit();
 };
 
-function update_diagram() {
-};
-
 $(document).ready(function($){
-  diagram = $('#diagram');
-  diagram.timer = null;
-
-  diagram.bind('keyup change', function(){
-    if (diagram.timer) clearTimeout(diagram.timer);
-    diagram.timer = setTimeout(update_diagram, 500);
-  });
-  update_diagram();
+    $('textarea').autoResizeTextAreaQ({"max_rows":40});
 });
