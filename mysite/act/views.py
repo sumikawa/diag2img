f4845a652b0de495b63570950c235679496f7015
Munechika Sumikawa <sumikawa@sumikawa.jp>
2011-08-04 11:23:02 +0900

    inherit block.views

--- mysite/act/views.py
+++ mysite/act/views.py
@@ -12,28 +12,7 @@ from django.utils import simplejson
 from django.views.decorators.csrf import csrf_exempt
 from ConfigParser import SafeConfigParser
 from actdiag import diagparser, builder, DiagramDraw
+from block.views import show, edit
 
 default_page = "QlpoOTFBWSZTWWnBhfsAACZdgEAQQAIACT4AIgUACiAASEijSPQagEqaGgB6SxHqj644FdKc63jZLbdjwYqMTM0HVNskiCZAwmF8XckU4UJBpwYX7A=="
 type = 'act'
-
-@cache_page(60 * 15)
-def show(request, diag):
-    tree = diagparser.parse(diagparser.tokenize(bz2.decompress(base64.b64decode(diag))))
-    diagram = builder.ScreenNodeBuilder.build(tree)
-    response = HttpResponse(mimetype='image/png')
-    draw = DiagramDraw.DiagramDraw('PNG', diagram, response, antialias=False, font=settings.FONT)
-    draw.draw()
-    draw.save()
-    return response
-
-@csrf_exempt
-def edit(request, diag):
-    if request.method == 'POST':
-        encode = base64.b64encode(bz2.compress(request.POST['diagram']))
-        data = simplejson.dumps([{"encode": encode},])
-        return HttpResponse(data, mimetype='application/json')
-    elif request.method == 'GET':
-        if diag == '':
-            return HttpResponseRedirect(reverse(settings.SITE_ROOT + type + '.views.edit', args=(default_page,)))
-        plain = bz2.decompress(base64.b64decode(diag))
-        return render_to_response('diag/edit.html', {'diag': diag, 'plain': plain, 'type': type})
