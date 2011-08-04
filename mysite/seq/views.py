afbe77ccec53d0c175e8cb6f5efd39560d32f795
Munechika Sumikawa <sumikawa@sumikawa.jp>
2011-08-04 12:57:06 +0900

    revert inherition

--- mysite/seq/views.py
+++ mysite/seq/views.py
@@ -12,7 +12,28 @@ from django.utils import simplejson
 from django.views.decorators.csrf import csrf_exempt
 from ConfigParser import SafeConfigParser
 from seqdiag import diagparser, builder, DiagramDraw
-from block.views import show, edit
 
 default_page = "QlpoOTFBWSZTWSLcEJkAAObfgEAQUAOADwKh3Ao+59/KMAD1tYJKRtU8k9RkHqANADTT1BIkUAyABoA0DR6gVSRA0ZNADQaABojAvzJx3iRERMo0El6TQVikjcE1aJo6jLmExpLsmb1BgY5xSwk4c56mnqhEqDyOLSxVSTkYq0pStWNpvckzg4uTCnWsiMCNMakdV4Ui9ag2EC1IIIRwLjE6IMYrgpMbGxolBZUteV6lWyooekXUpy+oLpKZmdQX8xiDGogKyCSjSQq0E1MVaBog9ZxxQjG18kGBTKKwUCrFzfxdyRThQkCLcEJk"
 type = 'seq'
+
+@cache_page(60 * 15)
+def show(request, diag):
+    tree = diagparser.parse(diagparser.tokenize(bz2.decompress(base64.b64decode(diag))))
+    diagram = builder.ScreenNodeBuilder.build(tree)
+    response = HttpResponse(mimetype='image/png')
+    draw = DiagramDraw.DiagramDraw('PNG', diagram, response, antialias=False, font=settings.FONT)
+    draw.draw()
+    draw.save()
+    return response
+
+@csrf_exempt
+def edit(request, diag):
+    if request.method == 'POST':
+        encode = base64.b64encode(bz2.compress(request.POST['diagram']))
+        data = simplejson.dumps([{"encode": encode},])
+        return HttpResponse(data, mimetype='application/json')
+    elif request.method == 'GET':
+        if diag == '':
+            return HttpResponseRedirect(reverse(settings.SITE_ROOT + type + '.views.edit', args=(default_page,)))
+        plain = bz2.decompress(base64.b64decode(diag))
+        return render_to_response('diag/edit.html', {'diag': diag, 'plain': plain, 'type': type})
